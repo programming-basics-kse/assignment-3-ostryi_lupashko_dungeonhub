800fd67f4a475024a01ed33f6a3411a8b610b700
Nimodimon <nimodimon@icloud.com>
2024-11-16 18:20:11 +0200

    Initial project structure

---
+++ src/methods/__init__.py
@@ -0,0 +1,6 @@
+from .interactive import process_interactive
+from .medals import process_medals
+from .overall import process_overall
+from .total import process_total
+
+__all__ = ['process_interactive', 'process_medals', 'process_overall', 'process_total']
---
+++ src/olympics.py
@@ -0,0 +1,4 @@
+from methods import *
+
+if __name__ == '__main__':
+    pass
