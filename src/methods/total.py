d53c612679f547177e59904c16b0e1c6e7891fe0
oostryi <osstryi@kse.org.ua>
2024-11-21 17:16:50 +0200

    Total method now returns blank line if in the year the games were not held

--- src/methods/total.py
+++ src/methods/total.py
@@ -1,15 +1,21 @@
-year = '1936'
+def process_total(year):
+    with open('../../data/Olympics.tsv', 'r') as file:
+        rows = []
+        info = {}
+        for line in file:
+            row = line.strip().split('\t')
+            if row[9] == year and row[14] != 'NA':
+                if row[6] not in info:
+                    info[row[6]] = {'Gold': 0, 'Silver': 0, 'Bronze': 0}
+                info[row[6]][row[14]] += 1
 
-with open('../../data/Olympics.tsv', 'r') as file:
-    rows = []
-    info = {}
-    for line in file:
-        row = line.strip().split('\t')
-        if row[9] == year and row[14] != 'NA':
-            if row[6] not in info:
-                info[row[6]] = {'Gold': 0, 'Silver': 0, 'Bronze': 0}
-            info[row[6]][row[14]] += 1
+    if info == {}:
+        return ""
 
-for country in info:
-    print(f"{country} - {info[country]['Gold']} - {info[country]['Silver']} - {info[country]['Bronze']}", end="\n\n")
+    result = ""
+    for country in info:
+        result += f"{country} - {info[country]['Gold']} - {info[country]['Silver']} - {info[country]['Bronze']}"
+        result += "\n\n"
+
+    return result
 #
