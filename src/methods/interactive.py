9133f5bac95363fc5d2001fb89a19c6c629a552f
Nimodimon <nimodimon@icloud.com>
2024-11-22 10:32:42 +0200

    Fixed problem with file

--- src/methods/interactive.py
+++ src/methods/interactive.py
@@ -1,9 +1,16 @@
 import csv
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(os.path.dirname(current_dir))  # Go up two levels to `project`
 
 def process_interactive() -> str:
     country = input("Enter country name: ")
 
-    with open('../../data/Olympics.tsv', 'r') as file:
+    file_path = os.path.join(project_root, "data", f"Olympics.tsv")
+    print(file_path)
+
+    with open(file_path, 'r') as file:
         reader = csv.reader(file, delimiter='\t')
         header = next(reader)
 
@@ -26,10 +33,11 @@ def process_interactive() -> str:
 
     result = "\n\n"
     result += "================"
+    result += "\n\n"
 
     first_olympiad_row = min(data, key=lambda x: int(x["year"]))
 
-    result += f"First olympiad for {country} was in {first_olympiad_row['year']} year in {first_olympiad_row['city']}."
+    result += f"First olympiad for {country} was in {first_olympiad_row['year']} year in {first_olympiad_row['city']}.\n"
 
     years_data = {}
 
@@ -37,27 +45,28 @@ def process_interactive() -> str:
         if row['year'] not in years_data:
             years_data[row['year']] = {"bronze": 0, "silver": 0, "gold": 0}
 
-        if row['Medal'] == "Bronze":
+        if row['medal'] == "Bronze":
             years_data[row['year']]["bronze"] += 1
-        elif row['Medal'] == "Silver":
+        elif row['medal'] == "Silver":
             years_data[row['year']]["silver"] += 1
-        elif row['Medal'] == "Gold":
+        elif row['medal'] == "Gold":
             years_data[row['year']]["gold"] += 1
 
     years_data = sorted(years_data.items(), key=lambda x: x[1]["gold"] + x[1]["silver"] + x[1]["bronze"])
     the_most_successful = years_data[-1]
     the_least_successful = years_data[0]
 
-    result += f"The most successful olympiad was in {the_most_successful[0]} year with {the_most_successful[1]['gold'] + the_most_successful[1]['silver'] + the_most_successful[1]['bronze']} medals."
-    result += f"The least successful olympiad was in {the_least_successful[0]} year with {the_least_successful[1]['gold'] + the_least_successful[1]['silver'] + the_least_successful[1]['bronze']} medals."
+    result += f"The most successful olympiad was in {the_most_successful[0]} year with {the_most_successful[1]['gold'] + the_most_successful[1]['silver'] + the_most_successful[1]['bronze']} medals.\n"
+    result += f"The least successful olympiad was in {the_least_successful[0]} year with {the_least_successful[1]['gold'] + the_least_successful[1]['silver'] + the_least_successful[1]['bronze']} medals.\n"
 
     bronze_average = sum(value["bronze"] for key, value in years_data) / len(years_data)
     silver_average = sum(value["silver"] for key, value in years_data) / len(years_data)
     gold_average = sum(value["gold"] for key, value in years_data) / len(years_data)
 
-    result = f"Bronze average: {bronze_average:.2f}, Silver average: {silver_average:.2f}, Gold average: {gold_average:.2f}"
+    result += f"Bronze average: {bronze_average:.2f}, Silver average: {silver_average:.2f}, Gold average: {gold_average:.2f}\n"
 
     result += "\n\n"
     result += "================"
+    result += "\n\n"
 
     return result
